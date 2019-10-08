4fad010fcd295aea6c2fa2902b7cae5d72c4244b
Ralph Squillace <ralph@squillace.com>
2019-10-07 20:00:21 -0400

    updating tests

--- pkg/helm3/version_test.go
+++ pkg/helm3/version_test.go
@@ -22,7 +22,7 @@ func TestPrintVersion(t *testing.T) {
 	m.PrintVersion(opts)
 
 	gotOutput := m.TestContext.GetOutput()
-	wantOutput := "helm3 v1.2.3 (abc123) by YOURNAME"
+	wantOutput := "helm3 v1.2.3 (abc123) by ralph squillace"
 	if !strings.Contains(gotOutput, wantOutput) {
 		t.Fatalf("invalid output:\nWANT:\t%q\nGOT:\t%q\n", wantOutput, gotOutput)
 	}
@@ -45,7 +45,7 @@ func TestPrintJsonVersion(t *testing.T) {
   "name": "helm3",
   "version": "v1.2.3",
   "commit": "abc123",
-  "author": "YOURNAME"
+  "author": "ralph squillace"
 }
 `
 	if !strings.Contains(gotOutput, wantOutput) {
